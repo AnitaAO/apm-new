b4348e64c86f4bc71e9ceee504a34f847c7ef6e0
Anita Ajah <anitaisbeta@gmail.com>
2021-12-02 21:25:39 +0100

    app module

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
+
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
     
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     errorMessage: string = '';
+    sub!: Subscription;
+
     private '_listFilter': string = '';
     get listFilter(): string {
         return this._listFilter;
@@ -39,7 +42,7 @@ export class ProductListComponent implements OnInit {
     }
     
     ngOnInit(): void {
-        this.productService.getProducts().subscribe({
+        this.sub = this.productService.getProducts().subscribe({
             next: products => {
                 this.products = products;
                 this.filteredProducts = this.products;
@@ -47,6 +50,10 @@ export class ProductListComponent implements OnInit {
             error: err => this.errorMessage = err
         });
         
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product List' + message;
